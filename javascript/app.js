7efc7b827f1c54e4cdde33555b357cb9946dfd55
FatimeMehmanli <fatimemhmnli@gmail.com>
2024-09-09 20:29:55 +0400

    salam

---
+++ javascript/app.js
@@ -0,0 +1,43 @@
+//  Task 01
+let text = "My name is Fatima";
+console.log(text);
+// 
+
+// Task 02 
+const name = "Fatima";
+console.log(name)
+// 
+
+
+// Task 08
+let nm = `"Sizin  adınız ` 
+let firstName = "Fatimə "
+let lastName = "Mehmanlı, "
+let greet = `Tanışlığımıza  şadam !" `;
+console.log(nm+ firstName + lastName + greet);
+// 
+// Task 09
+let nma = `"Sizi bir daha görməyimizə görə şadıq,`
+let firstNm = "Fatimə"
+let lastNm = "Mehmanlı"
+console.log(nma + firstNm + " " + lastNm);
+
+// Task 10
+let FullName = "Fatimə Mehmanlı";
+console.log(FullName.length + 1)
+
+// Task 11
+let flnm = "Fatima Mehmanli";
+console.log(flnm[0]);
+console.log(flnm[7]);
+ 
+// Task 12
+let FULLNAME =prompt("Zəhmət olmasa adınızı və soyadınızı qeyd edin")
+let spaceIndex = FULLNAME.indexOf("")
+let firstnameS = FULLNAME.slice(0, spaceIndex);
+let lastNameS = FULLNAME.slice(spaceIndex + 1);
+console.log(`${lastNameS} + ${firstnameS}`);
+
+// Task 13 
+let nameF = prompt(`Salam,adinizi daxil edin.`);
+console.log(nameF);
